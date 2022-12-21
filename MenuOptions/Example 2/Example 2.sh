a40a1d7980e73faf6257d6fece6e8a188cc04dd2
DiscDuck <github@moll.one>
2022-12-21 11:23:20 +0100

    add examples

---
+++ MenuOptions/Example 2/Example 2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+######################################################################
+# Title   : IBRAMENU Personal - Example 2
+# By      : DiscDuck
+# License : General Public License GPL-3.0-or-later
+# Another fine product brought to you by IBRACORP™
+######################################################################
+
+# Include ibrafunc for all the awesome functions
+source /opt/ibracorp/ibramenu/ibrafunc.sh
+
+msgbox "Example 2"
